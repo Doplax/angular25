fc5f4c7c17f9747e3e8a87968c44c54bb1ef8012
Doplax <doplax@gmail.com>
2025-04-08 11:55:44 +0200

    feat: @if - Mostrar elementos de forma condicional

--- 02-bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
@@ -23,7 +23,7 @@ export class DragonballPageComponent {
     { id: 4, name: 'Piccolo', power: 6000 },
     { id: 5, name: 'Krillin', power: 5000 },
     { id: 6, name: 'Bulma', power: 100 },
-    { id: 7, name: 'Yamcha', power: 4000 },
+    { id: 7, name: 'Yamcha', power: 500 },
     { id: 8, name: 'Tien', power: 4500 },
     { id: 9, name: 'Chiaotzu', power: 3000 },
     { id: 10, name: 'Master Roshi', power: 2000 },
