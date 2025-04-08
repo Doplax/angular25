878ace3fa30ebd7d1cacb426581175153529cee3
Doplax <doplax@gmail.com>
2025-04-08 15:40:20 +0200

    feat: LinkedSignal - Cargar del LocalStorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,20 +1,16 @@
 import { effect, Injectable, signal } from "@angular/core";
 import { Character } from "../interfaces";
 
+
+function loadFromLocalStorage():Character[] {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+}
+
 @Injectable({providedIn: 'root'}) // Provides the service in the root injector
 export class DragonBallService {
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9000 },
-    { id: 2, name: 'Vegeta', power: 8500 },
-    { id: 3, name: 'Gohan', power: 7000 },
-    { id: 4, name: 'Piccolo', power: 6000 },
-    { id: 5, name: 'Krillin', power: 5000 },
-    { id: 6, name: 'Bulma', power: 100 },
-    { id: 7, name: 'Yamcha', power: 500 },
-    { id: 8, name: 'Tien', power: 4500 },
-    { id: 9, name: 'Chiaotzu', power: 3000 },
-    { id: 10, name: 'Master Roshi', power: 2000 },
-  ])
+
+  characters = signal<Character[]>(loadFromLocalStorage());
 
   // Este efecto se ejecuta cada vez que la señal cambia
   saveToLocalStorage = effect(()=>{
@@ -28,3 +24,17 @@ export class DragonBallService {
     });
   }
 }
+
+// Por si se borra la lista
+//characters = signal<Character[]>([
+//  { id: 1, name: 'Goku', power: 9000 },
+//  { id: 2, name: 'Vegeta', power: 8500 },
+//  { id: 3, name: 'Gohan', power: 7000 },
+//  { id: 4, name: 'Piccolo', power: 6000 },
+//  { id: 5, name: 'Krillin', power: 5000 },
+//  { id: 6, name: 'Bulma', power: 100 },
+//  { id: 7, name: 'Yamcha', power: 500 },
+//  { id: 8, name: 'Tien', power: 4500 },
+//  { id: 9, name: 'Chiaotzu', power: 3000 },
+//  { id: 10, name: 'Master Roshi', power: 2000 },
+//])
