d7867ac1b8ee95f03397aabe8afd4dc4621095ee
Doplax <doplax@gmail.com>
2025-04-08 11:30:25 +0200

    feat: @for - Realizar iteraciones de elementos

--- 02-bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
 
 @Component({
   selector: 'app-dragonball-page',
@@ -6,4 +13,19 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball-page.component.html',
   styleUrl: './dragonball-page.component.css',
 })
-export class DragonballPageComponent { }
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9000 },
+    { id: 2, name: 'Vegeta', power: 8500 },
+    { id: 3, name: 'Gohan', power: 7000 },
+    { id: 4, name: 'Piccolo', power: 6000 },
+    { id: 5, name: 'Krillin', power: 5000 },
+    { id: 6, name: 'Bulma', power: 100 },
+    { id: 7, name: 'Yamcha', power: 4000 },
+    { id: 8, name: 'Tien', power: 4500 },
+    { id: 9, name: 'Chiaotzu', power: 3000 },
+    { id: 10, name: 'Master Roshi', power: 2000 },
+  ])
+
+}
